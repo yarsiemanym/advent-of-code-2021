0869d3179f27f3cd1c7fafbbfd8286d138481706
Ray Saltrelli <ray@saltrelli.com>
2021-12-05 12:58:57 -0500

    Handle a bad configuration because reasons.

--- common/logging.go
+++ common/logging.go
@@ -7,11 +7,18 @@ import (
 )
 
 func InitLogging() {
-	logLevel, err := log.ParseLevel(os.Getenv("AOC_LOG_LEVEL"))
+	envVar := os.Getenv("AOC_LOG_LEVEL")
 
-	if err != nil {
-		logLevel = log.WarnLevel
+	if envVar != "" {
+		logLevel, err := log.ParseLevel(os.Getenv("AOC_LOG_LEVEL"))
+
+		if err != nil {
+			log.SetLevel(log.WarnLevel)
+			log.Warnf("\"%v\" is not a valid log level. Falling back to \"warn\".", envVar)
+		} else {
+			log.SetLevel(logLevel)
+		}
+	} else {
+		log.SetLevel(log.WarnLevel)
 	}
-
-	log.SetLevel(logLevel)
 }
