29814ea6fbd0bec90f3e0225d5cdc2129c53e4af
Ray Saltrelli <ray@saltrelli.com>
2021-12-09 12:09:17 -0500

    Day 9 complete.

--- common/bounded_plane.go
+++ common/bounded_plane.go
@@ -1,7 +1,17 @@
 package common
 
+import log "github.com/sirupsen/logrus"
+
 type BoundedPlane struct {
-	span *LineSegment
+	span      *LineSegment
+	locations [][]interface{}
+}
+
+func NewBoundedPlane(height int, width int) *BoundedPlane {
+	return &BoundedPlane{
+		span:      NewLineSegment(NewPoint(0, 0), NewPoint(width-1, height-1)),
+		locations: initializeLocations(height, width),
+	}
 }
 
 func NewBoundedPlaneFromPoints(points []*Point) *BoundedPlane {
@@ -18,7 +28,8 @@ func NewBoundedPlaneFromPoints(points []*Point) *BoundedPlane {
 	}
 
 	return &BoundedPlane{
-		span: NewLineSegment(NewPoint(minX, minY), NewPoint(maxX, maxY)),
+		span:      NewLineSegment(NewPoint(minX, minY), NewPoint(maxX, maxY)),
+		locations: initializeLocations(maxY-minY, maxX-minX),
 	}
 }
 
@@ -36,10 +47,76 @@ func NewBoundedPlaneFromLines(lines []*LineSegment) *BoundedPlane {
 	}
 
 	return &BoundedPlane{
-		span: NewLineSegment(NewPoint(minX, minY), NewPoint(maxX, maxY)),
+		span:      NewLineSegment(NewPoint(minX, minY), NewPoint(maxX, maxY)),
+		locations: initializeLocations(maxY-minY, maxX-minX),
 	}
+}
+
+func initializeLocations(height int, width int) [][]interface{} {
+	locations := make([][]interface{}, height)
+
+	for row := range locations {
+
+		locations[row] = make([]interface{}, width)
+	}
+
+	return locations
 }
 
 func (plane *BoundedPlane) Span() *LineSegment {
 	return plane.span
+}
+
+func (plane *BoundedPlane) GetValueAt(point *Point) interface{} {
+	row := point.y - plane.span.start.y
+	col := point.x - plane.span.start.x
+	return plane.locations[row][col]
+}
+
+func (plane *BoundedPlane) SetValueAt(point *Point, value interface{}) {
+	row := point.y - plane.span.start.y
+	col := point.x - plane.span.start.x
+	plane.locations[row][col] = value
+}
+
+func (plane *BoundedPlane) GetPointsAdjacentTo(point *Point) []*Point {
+	log.Tracef("Getting all points adjacent to %v.", *point)
+
+	var adjacentPoints []*Point
+
+	if point.x+1 <= plane.span.end.x {
+		adjacentPoints = append(adjacentPoints, NewPoint(point.x+1, point.y))
+	}
+
+	if point.y+1 <= plane.span.end.y {
+		adjacentPoints = append(adjacentPoints, NewPoint(point.x, point.y+1))
+	}
+
+	if point.x-1 >= plane.span.start.x {
+		adjacentPoints = append(adjacentPoints, NewPoint(point.x-1, point.y))
+	}
+
+	if point.y-1 >= plane.span.start.y {
+		adjacentPoints = append(adjacentPoints, NewPoint(point.x, point.y-1))
+	}
+
+	log.Tracef("%v adjacent points.", len(adjacentPoints))
+
+	return adjacentPoints
+}
+
+func (plane *BoundedPlane) GetAllPoints() []*Point {
+	log.Tracef("Getting all points in bounded plane.")
+
+	var points []*Point
+
+	for y := plane.span.start.y; y <= plane.span.end.y; y++ {
+		for x := plane.span.start.x; x <= plane.span.end.x; x++ {
+			points = append(points, NewPoint(x, y))
+		}
+	}
+
+	log.Tracef("%v points.", len(points))
+
+	return points
 }
--- day09/solution.go
+++ day09/solution.go
@@ -1,36 +1,166 @@
 package day09
 
 import (
+	"sort"
+	"strconv"
+
 	log "github.com/sirupsen/logrus"
 	"github.com/yarsiemanym/advent-of-code-2021/common"
 )
 
 func Solve(puzzle *common.Puzzle) common.Answer {
 
-	// TODO: parse input
+	text := common.ReadFile(puzzle.InputFile)
+	heightMap := parseHeightMap(text)
 
 	return common.Answer{
 		Year:  puzzle.Year,
 		Day:   puzzle.Day,
-		Part1: solvePart1(),
-		Part2: solvePart2(),
+		Part1: solvePart1(heightMap),
+		Part2: solvePart2(heightMap),
 	}
 }
 
-func solvePart1() string {
+func solvePart1(heightMap *common.BoundedPlane) string {
 	log.Info("Solving part 1.")
 
-	// TODO: implement part 1 solution
+	lowPoints := findLowPoints(heightMap)
+	log.Debugf("%v low points found.", len(lowPoints))
+	totalRiskLevel := 0
+
+	for index, lowPoint := range lowPoints {
+		log.Debugf("Assessing risk level of lowpoint %v.", index)
+		log.Tracef("lowPoint = %v", *lowPoint)
+		risklevel := 1 + heightMap.GetValueAt(lowPoint).(int)
+		log.Tracef("risklevel = %v", risklevel)
+		totalRiskLevel += risklevel
+	}
+
+	log.Tracef("totalRiskLevel = %v", totalRiskLevel)
 
 	log.Info("Part 1 solved.")
-	return "Not implemented."
+	return strconv.Itoa(totalRiskLevel)
 }
 
-func solvePart2() string {
+func findLowPoints(heightMap *common.BoundedPlane) []*common.Point {
+	var lowPoints []*common.Point
+
+	for _, point := range heightMap.GetAllPoints() {
+		if isLowPoint(point, heightMap) {
+			lowPoints = append(lowPoints, point)
+		}
+	}
+
+	return lowPoints
+}
+
+func isLowPoint(point *common.Point, heightMap *common.BoundedPlane) bool {
+	currentValue := heightMap.GetValueAt(point).(int)
+	adjacentPoints := heightMap.GetPointsAdjacentTo(point)
+	isLow := true
+
+	for _, adjacentPoint := range adjacentPoints {
+		adjacentValue := heightMap.GetValueAt(adjacentPoint).(int)
+		if adjacentValue <= currentValue {
+			isLow = false
+			break
+		}
+	}
+
+	return isLow
+}
+
+func solvePart2(heightMap *common.BoundedPlane) string {
 	log.Info("Solving part 2.")
 
-	// TODO: implement part 2 solution
+	lowPoints := findLowPoints(heightMap)
+	basinSizes := make([]int, len(lowPoints))
+
+	for index, lowPoint := range lowPoints {
+		basinSizes[index] = len(exploreBasin(lowPoint, heightMap, make([]*common.Point, 0)))
+	}
+
+	sort.Ints(basinSizes)
+	length := len(basinSizes)
+
+	log.Tracef("basinSizes = %v", basinSizes)
+	product := basinSizes[length-1] * basinSizes[length-2] * basinSizes[length-3]
+	log.Tracef("product = %v", product)
 
 	log.Info("Part 2 solved.")
-	return "Not implemented."
+	return strconv.Itoa(product)
+}
+
+func exploreBasin(lowPoint *common.Point, heightMap *common.BoundedPlane, alreadyExplored []*common.Point) []*common.Point {
+	log.Debugf("Looking for paths uphill from point %v.", *lowPoint)
+	currentValue := heightMap.GetValueAt(lowPoint).(int)
+	adjacentPoints := heightMap.GetPointsAdjacentTo(lowPoint)
+	pointsInBasin := []*common.Point{
+		lowPoint,
+	}
+
+	for _, adjacentPoint := range adjacentPoints {
+		if sliceContainsPoint(alreadyExplored, adjacentPoint) {
+			log.Tracef("Already explored point %v. Skipping.", *adjacentPoint)
+			continue
+		}
+
+		adjacentValue := heightMap.GetValueAt(adjacentPoint).(int)
+
+		if adjacentValue < 9 && adjacentValue >= currentValue {
+			log.Tracef("Point %v is uphill. Climbing.", *adjacentPoint)
+			uphillPoints := exploreBasin(adjacentPoint, heightMap, pointsInBasin)
+
+			for _, uphillPoint := range uphillPoints {
+				if !sliceContainsPoint(pointsInBasin, uphillPoint) {
+					pointsInBasin = append(pointsInBasin, uphillPoint)
+				}
+			}
+		}
+	}
+
+	return pointsInBasin
+}
+
+func sliceContainsPoint(slice []*common.Point, element *common.Point) bool {
+	for _, member := range slice {
+		if *member == *element {
+			return true
+		}
+	}
+
+	return false
+}
+
+func parseHeightMap(text string) *common.BoundedPlane {
+	lines := common.Split(text, "\n")
+	var heightMap [][]int
+
+	for row, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		heightMap = append(heightMap, make([]int, len(line)))
+
+		for col, char := range line {
+			height, err := strconv.Atoi(string(char))
+
+			if err != nil {
+				log.Fatalf("'%c' is not an integer.", char)
+			}
+
+			heightMap[row][col] = height
+		}
+	}
+
+	plane := common.NewBoundedPlane(len(heightMap), len(heightMap[0]))
+
+	for row := range heightMap {
+		for col := range heightMap[row] {
+			plane.SetValueAt(common.NewPoint(col, row), heightMap[row][col])
+		}
+	}
+
+	return plane
 }
